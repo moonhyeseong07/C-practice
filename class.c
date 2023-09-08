93f964acfd5c95b386bad75e4e4ab4cfce0f872e
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-09-08 21:11:42 +0900

    9/8

--- class.c
+++ class.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
-
-int main()
-{
-	int i,n,c;
-	int a[10000];
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	c=a[1];
-	for(i=1;i<=n;i++)
-	{
-		if(a[i]<c)
-		{
-			c=a[i];
-		}
-	}
-	printf("%d\n",c);
-	return 0;
+main(){
+char a[] ={'A', 'B','C','D','E','F'};
+char* p;
+p = &a[2];
+   printf("%c, %c\n", *p, *(p-2));
 }
