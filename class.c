c4fed77ed441492bdac0782cd502594069dd3960
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-09-09 23:35:15 +0900

    9/9

--- class.c
+++ class.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-main(){
-char a[] ={'A', 'B','C','D','E','F'};
-char* p;
-p = &a[2];
-   printf("%c, %c\n", *p, *(p-2));
+void swap(int* a, int* b){
+   int temp;
+   temp = *a;
+   *a=*b;
+   *b = temp;
+}
+
+int main(){
+int a=10, b=20;
+swap(&a, &b);
+return 0;
 }
