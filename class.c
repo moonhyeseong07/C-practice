e5d07fc729d2f409b9e61a62b0397b3aa5cc3c74
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-04 15:29:00 +0900

    10/4

--- class.c
+++ class.c
@@ -1,12 +1,78 @@
-void swap(int* a, int* b){
-   int temp;
-   temp = *a;
-   *a=*b;
-   *b = temp;
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define BUCKETS 10
+#define DIGITS 3
+#define SIZE 10
+typedef int element;
+typedef struct {
+element data[MAX];
+   int front,rear;
+} QueueType;
+
+void error(char* message)
+{
+   fprintf(stderr, "%s\n",message);
+exit(1);
 }
 
-int main(){
-int a=10, b=20;
-swap(&a, &b);
+void init_queue(QueueType* q)
+{
+   q->front=q->rear=0;
+}
+
+int is_empty(QueueType* q)
+{
+   return (q->front == q->rear);
+}
+
+int is_full(QueueType* q)
+{
+   return ((q->rear+1)%MAX==q->front);
+}
+
+void enqueue(QueueType* q,element item)
+{
+   if(is_full(q)) error("큐가 포화상태입니다");
+   q->rear=(q->rear+1)%MAX;
+   q->data[q->rear]=item;
+}
+
+element dequeue(QueueType* q)
+{
+   if(is_empty(q)) error("큐가 공백상태입니다");
+   q->front=(q->front+1)%MAX;
+   return q->data[q->front];
+}
+
+void radix_sort(int list[],int n)
+{
+   int i,b,d,factor=1;
+   QueueType queues[BUCKETS];
+   for(b=0;b<BUCKETS;++b) init_queue(&queues[b]);
+   for(d=0;d<DIGITS;++d){
+      for(i=0;i<n;++i) enqueue(&queues[(list[i]/factor)%10],list[i]);
+      for(b=i=0;b<BUCKETS;++b){
+         while (!is_empty(&queues[b])){
+            list[i++]=dequeue(&queues[b]);
+      }
+   }
+   factor*=10;
+   for(int k=0;k<n;++k){
+      printf("%d ",list[k]);
+   }
+   printf("\n");
+}
+}
+
+int main(void) {
+   int list[]={412,73,125,221,137,82,194,367,150,256};
+   int n=sizeof(list)/sizeof(int);
+   radix_sort(list, SIZE);
+   for(int i=0;i<SIZE;++i){
+      printf("%d ",list[i]);
+}
+printf("\n"); 
 return 0;
 }
