982d384ddc971a427296893083e37dcfe6d8aa20
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-05 20:24:12 +0900

    10/5

--- class.c
+++ class.c
@@ -1,78 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define MAX 100
-#define BUCKETS 10
-#define DIGITS 3
-#define SIZE 10
-typedef int element;
+#define MAX_STACK_SIZE 100
+
+typedef int element; // int를 element로 재정의
+
 typedef struct {
-element data[MAX];
-   int front,rear;
-} QueueType;
+    int top;            // 스택의 현재 위치를 나타내는 변수
+    element data[MAX_STACK_SIZE]; // 스택 데이터를 저장하는 배열
+} Stack;
 
-void error(char* message)
-{
-   fprintf(stderr, "%s\n",message);
-exit(1);
+// 전역변수로 스택 구조체 선언
+Stack s;
+
+// 스택 초기화 함수
+void initStack() {
+    s.top = -1; // 스택을 초기화하고 top을 -1로 설정
 }
 
-void init_queue(QueueType* q)
-{
-   q->front=q->rear=0;
+// 스택이 비어있는지 확인하는 함수
+bool isEmpty() {
+   if (s.top == -1)
+        return true; // top이 -1이면 스택이 비어있음
+   return false;
 }
 
-int is_empty(QueueType* q)
-{
-   return (q->front == q->rear);
+// 스택이 꽉 차 있는지 확인하는 함수
+bool isFull() {
+   if (s.top == MAX_STACK_SIZE - 1)
+        return true; // top이 배열의 최대 크기와 같으면 스택이 꽉 차있음
+   return false;
 }
 
-int is_full(QueueType* q)
-{
-   return ((q->rear+1)%MAX==q->front);
-}
-
-void enqueue(QueueType* q,element item)
-{
-   if(is_full(q)) error("큐가 포화상태입니다");
-   q->rear=(q->rear+1)%MAX;
-   q->data[q->rear]=item;
-}
-
-element dequeue(QueueType* q)
-{
-   if(is_empty(q)) error("큐가 공백상태입니다");
-   q->front=(q->front+1)%MAX;
-   return q->data[q->front];
-}
-
-void radix_sort(int list[],int n)
-{
-   int i,b,d,factor=1;
-   QueueType queues[BUCKETS];
-   for(b=0;b<BUCKETS;++b) init_queue(&queues[b]);
-   for(d=0;d<DIGITS;++d){
-      for(i=0;i<n;++i) enqueue(&queues[(list[i]/factor)%10],list[i]);
-      for(b=i=0;b<BUCKETS;++b){
-         while (!is_empty(&queues[b])){
-            list[i++]=dequeue(&queues[b]);
-      }
+// 스택에 원소를 추가하는 함수
+void push(element item) {
+   if (isFull()) {
+      printf("스택이 꽉 차있습니다. \n");
+      return;
    }
-   factor*=10;
-   for(int k=0;k<n;++k){
-      printf("%d ",list[k]);
-   }
-   printf("\n");
-}
+    s.data[++s.top] = item; // top을 증가시키고 해당 위치에 원소 추가
 }
 
-int main(void) {
-   int list[]={412,73,125,221,137,82,194,367,150,256};
-   int n=sizeof(list)/sizeof(int);
-   radix_sort(list, SIZE);
-   for(int i=0;i<SIZE;++i){
-      printf("%d ",list[i]);
+// 스택에서 원소를 제거하고 반환하는 함수
+element pop() {
+   if (isEmpty()) {
+      printf("스택이 비었습니다. \n");
+      return 0;
+   }
+    return s.data[s.top--]; // top 위치의 원소를 반환하고 top을 감소시킴
 }
-printf("\n"); 
-return 0;
+
+// 스택의 맨 위에 있는 원소를 반환하는 함수
+element peek() {
+   if (isEmpty()) {
+      printf("스택이 비었습니다. \n");
+      return 0;
+   }
+    return s.data[s.top]; // top 위치의 원소를 반환하지만 top은 변경하지 않음
+}
+
+int main() {
+    initStack(); // 스택 초기화
+    push(1);     // 1을 스택에 추가
+    push(2);     // 2를 스택에 추가
+    push(3);     // 3을 스택에 추가
+    printf("%d\n", pop());  // 스택에서 원소를 제거하고 출력
+    printf("%d\n", peek()); // 스택의 맨 위 원소를 출력
+    printf("%d\n", pop());  // 스택에서 원소를 제거하고 출력
+   return 0;
 }
