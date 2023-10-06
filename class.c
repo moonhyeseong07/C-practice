6dc2a8a56904ebae0c992e86d6678af7bd83fa9c
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-06 20:49:32 +0900

    10/6

--- class.c
+++ class.c
@@ -1,72 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdlib.h> // abs 함수를 사용하기 위함
 
-#define MAX_STACK_SIZE 100
+// 좌표를 나타내는 구조체 정의
+typedef struct point {
+    int x, y; // x와 y 좌표를 가짐
+} POINT;
 
-typedef int element; // int를 element로 재정의
-
-typedef struct {
-    int top;            // 스택의 현재 위치를 나타내는 변수
-    element data[MAX_STACK_SIZE]; // 스택 데이터를 저장하는 배열
-} Stack;
-
-// 전역변수로 스택 구조체 선언
-Stack s;
-
-// 스택 초기화 함수
-void initStack() {
-    s.top = -1; // 스택을 초기화하고 top을 -1로 설정
-}
-
-// 스택이 비어있는지 확인하는 함수
-bool isEmpty() {
-   if (s.top == -1)
-        return true; // top이 -1이면 스택이 비어있음
-   return false;
-}
-
-// 스택이 꽉 차 있는지 확인하는 함수
-bool isFull() {
-   if (s.top == MAX_STACK_SIZE - 1)
-        return true; // top이 배열의 최대 크기와 같으면 스택이 꽉 차있음
-   return false;
-}
-
-// 스택에 원소를 추가하는 함수
-void push(element item) {
-   if (isFull()) {
-      printf("스택이 꽉 차있습니다. \n");
-      return;
-   }
-    s.data[++s.top] = item; // top을 증가시키고 해당 위치에 원소 추가
-}
-
-// 스택에서 원소를 제거하고 반환하는 함수
-element pop() {
-   if (isEmpty()) {
-      printf("스택이 비었습니다. \n");
-      return 0;
-   }
-    return s.data[s.top--]; // top 위치의 원소를 반환하고 top을 감소시킴
-}
-
-// 스택의 맨 위에 있는 원소를 반환하는 함수
-element peek() {
-   if (isEmpty()) {
-      printf("스택이 비었습니다. \n");
-      return 0;
-   }
-    return s.data[s.top]; // top 위치의 원소를 반환하지만 top은 변경하지 않음
-}
+// 함수 원형 선언
+void inputPoint(POINT *p); // 좌표 입력 함수
+void outputPoint(POINT *p); // 좌표 출력 함수
+void distance(POINT *p, POINT *q); // 원점에서 가장 가까운 좌표 찾는 함수
 
 int main() {
-    initStack(); // 스택 초기화
-    push(1);     // 1을 스택에 추가
-    push(2);     // 2를 스택에 추가
-    push(3);     // 3을 스택에 추가
-    printf("%d\n", pop());  // 스택에서 원소를 제거하고 출력
-    printf("%d\n", peek()); // 스택의 맨 위 원소를 출력
-    printf("%d\n", pop());  // 스택에서 원소를 제거하고 출력
+    POINT p[5]; // POINT 구조체 배열 정의
+    POINT origin = {999999, 999999}; // 최댓값 찾기 위해 초기값 설정
+
+    inputPoint(p); // 사용자로부터 좌표를 입력받음
+    distance(p, &origin); // 원점과 가장 가까운 좌표 찾음
+   printf("원점에서 가장 가까운 좌표 : ");
+    outputPoint(&origin); // 가장 가까운 좌표를 출력
+
    return 0;
+}
+
+// 사용자로부터 좌표를 입력받는 함수
+void inputPoint(POINT *p) {
+   for (int i = 0; i < 5; ++i) {
+      scanf("%d %d", &p[i].x, &p[i].y);
+   }
+}
+
+// 좌표를 출력하는 함수
+void outputPoint(POINT *p) {
+   printf("(%4d,%5d)", p->x, p->y);
+}
+
+// 원점에서 가장 가까운 좌표를 찾는 함수
+void distance(POINT *p, POINT *q) {
+   for (int i = 0; i < 5; ++i) {
+        int now = abs(p[i].x + p[i].y); // 현재 좌표의 절대값 합 구하기
+        int qsum = abs(q->x + q->y); // 현재까지 가장 가까운 좌표의 절대값 합 구하기
+
+      if (now < qsum) {
+            // 현재 좌표가 현재까지 가장 가까운 좌표보다 가까울 때 업데이트
+            q->x = p[i].x;
+            q->y = p[i].y;
+      } else if (now == qsum && (p[i].x + p[i].y) < (q->x + q->y)) {
+            // 절대값 합이 같을 때, 좌표의 합을 비교하여서 더 작은 값 변경
+            q->x = p[i].x;
+            q->y = p[i].y;
+      }
+   }
 }
