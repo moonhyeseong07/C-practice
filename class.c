65601736f350b6e4224f1a09c95c57ebe077b302
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-07 22:59:50 +0900

    10/7

--- class.c
+++ class.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h> // abs 함수를 사용하기 위함
+#define ROWS 3
+#define COLS 3
 
-// 좌표를 나타내는 구조체 정의
-typedef struct point {
-    int x, y; // x와 y 좌표를 가짐
-} POINT;
-
-// 함수 원형 선언
-void inputPoint(POINT *p); // 좌표 입력 함수
-void outputPoint(POINT *p); // 좌표 출력 함수
-void distance(POINT *p, POINT *q); // 원점에서 가장 가까운 좌표 찾는 함수
+void inputMatrix(int a[][COLS]);
+void transMatrix(int a[][COLS], int b[][COLS]);
+void printMatrix(int a[][COLS]);
 
 int main() {
-    POINT p[5]; // POINT 구조체 배열 정의
-    POINT origin = {999999, 999999}; // 최댓값 찾기 위해 초기값 설정
-
-    inputPoint(p); // 사용자로부터 좌표를 입력받음
-    distance(p, &origin); // 원점과 가장 가까운 좌표 찾음
-   printf("원점에서 가장 가까운 좌표 : ");
-    outputPoint(&origin); // 가장 가까운 좌표를 출력
-
+   int array1[ROWS][COLS];
+   int array2[ROWS][COLS];
+   
+   inputMatrix(array1);
+   transMatrix(array1, array2);
+   
+   printMatrix(array1);
+   
+   printf("\n   전치행렬 출력\n");
+   printf("  --------------\n");
+   printMatrix(array2);
+   
    return 0;
 }
 
-// 사용자로부터 좌표를 입력받는 함수
-void inputPoint(POINT *p) {
-   for (int i = 0; i < 5; ++i) {
-      scanf("%d %d", &p[i].x, &p[i].y);
+void inputMatrix(int a[][COLS]) {
+   int i, j;
+   for (i = 0; i < ROWS; i++) {
+      for (j = 0; j < COLS; j++) {
+            scanf("%d", &a[i][j]);
+      }
    }
 }
 
-// 좌표를 출력하는 함수
-void outputPoint(POINT *p) {
-   printf("(%4d,%5d)", p->x, p->y);
+void transMatrix(int a[][COLS], int b[][COLS]) {
+   int i, j;
+   for (i = 0; i < ROWS; i++) {
+      for (j = 0; j < COLS; j++) {
+            b[j][i] = a[i][j];
+      }
+   }
 }
 
-// 원점에서 가장 가까운 좌표를 찾는 함수
-void distance(POINT *p, POINT *q) {
-   for (int i = 0; i < 5; ++i) {
-        int now = abs(p[i].x + p[i].y); // 현재 좌표의 절대값 합 구하기
-        int qsum = abs(q->x + q->y); // 현재까지 가장 가까운 좌표의 절대값 합 구하기
-
-      if (now < qsum) {
-            // 현재 좌표가 현재까지 가장 가까운 좌표보다 가까울 때 업데이트
-            q->x = p[i].x;
-            q->y = p[i].y;
-      } else if (now == qsum && (p[i].x + p[i].y) < (q->x + q->y)) {
-            // 절대값 합이 같을 때, 좌표의 합을 비교하여서 더 작은 값 변경
-            q->x = p[i].x;
-            q->y = p[i].y;
+void printMatrix(int a[][COLS]) {
+   int i, j;
+   for (i = 0; i < ROWS; i++) {
+      for (j = 0; j < COLS; j++) {
+            printf("%c   %d", (a[i][j] >= 0) ? ' ' : '-',abs(a[i][j]));
       }
+      printf("\n");
    }
 }
