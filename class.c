e0eece8fd8b93bc650a6a83bdc361559ca48d55f
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-11 22:59:29 +0900

    10/11

--- class.c
+++ class.c
@@ -1,51 +1,13 @@
 #include <stdio.h>
-#define ROWS 3
-#define COLS 3
-
-void inputMatrix(int a[][COLS]);
-void transMatrix(int a[][COLS], int b[][COLS]);
-void printMatrix(int a[][COLS]);
-
 int main() {
-   int array1[ROWS][COLS];
-   int array2[ROWS][COLS];
-   
-   inputMatrix(array1);
-   transMatrix(array1, array2);
-   
-   printMatrix(array1);
-   
-   printf("\n   전치행렬 출력\n");
-   printf("  --------------\n");
-   printMatrix(array2);
-   
-   return 0;
-}
-
-void inputMatrix(int a[][COLS]) {
-   int i, j;
-   for (i = 0; i < ROWS; i++) {
-      for (j = 0; j < COLS; j++) {
-            scanf("%d", &a[i][j]);
-      }
-   }
-}
-
-void transMatrix(int a[][COLS], int b[][COLS]) {
-   int i, j;
-   for (i = 0; i < ROWS; i++) {
-      for (j = 0; j < COLS; j++) {
-            b[j][i] = a[i][j];
-      }
-   }
-}
-
-void printMatrix(int a[][COLS]) {
-   int i, j;
-   for (i = 0; i < ROWS; i++) {
-      for (j = 0; j < COLS; j++) {
-            printf("%c   %d", (a[i][j] >= 0) ? ' ' : '-',abs(a[i][j]));
-      }
-      printf("\n");
-   }
+	int n,m,i,sum=0;
+	scanf("%d %d",&n,&m);
+	for(i=1;i<=n;i++)
+	{
+		if(i%m==0){
+			sum+=i;
+		}
+	}
+	printf("%d",sum);
+	return 0;
 }
