f1c9b8ec2f052c2670c938473d6583787ad65374
moonhyeseong2007 <moonhyeseong2007@gmail.com>
2023-10-19 19:16:24 +0900

    10/19

--- class.c
+++ class.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
-int main() {
-	int n,k,min=100,max=0;
-	scanf("%d",&n);
-	for(int i=0;i<n;++i){
-		scanf("%d",&k);
-		if(max<k)
-			max=k;
-		if(min>k)
-			min=k;
+int i,k,n=8;
+int doublehash(int key)
+{
+	if(key>20) return 4;
+	else return 5;
+} 
+int hash(int key)
+{
+	return key%n;
+}
+int main()
+{
+	int key;
+	int list[8]={0,0,10,3,2,5,0,0};
+	scanf("%d",&key);
+	int index=hash(key);
+	while(1)
+	{
+	if(list[index]==0)
+	{
+		list[index]=key;
+		break;
 	}
-		printf("%d",max-min);
+		else{
+			k++;
+			index=(hash(key)+doublehash(key)*k)%n;
+		}
+	}
+	printf("%d",index);
 	return 0;
 }
